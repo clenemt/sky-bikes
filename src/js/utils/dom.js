40c25f80fdff74f3205e660fe9d453f8fbcc788d
clenemt <clement.moron@gmail.com>
2018-06-17 15:54:14 +0200

    :sparkles: Add dom utils

---
+++ src/js/utils/dom.js
@@ -0,0 +1,45 @@
+/* eslint arrow-body-style: off, no-param-reassign: off */
+
+class Dom {
+  constructor(s) {
+    if (typeof s === 'string')
+      this.value = Array.prototype.slice.call(document.querySelectorAll(s));
+    if (typeof s === 'object') this.value = [s];
+  }
+
+  eq = (n) => {
+    this.value = [this.value[n]];
+    return this;
+  };
+
+  each = (fn) => {
+    [].forEach.call(this.value, fn);
+    return this;
+  };
+
+  css = (v) => this.each((i) => (i.style.cssText += v));
+  cssdom = (v) => this.each((i) => Object.keys(v).forEach((key) => (i.style[key] = v[key])));
+
+  attr = (a, v) => this.each((i) => i.setAttribute(a, v));
+  getAttr = (v) => this.value[0].getAttribute(v);
+  removeAttr = (v) => this.each((i) => i.removeAttribute(v));
+
+  on = (type, fn) => this.each((i) => i.addEventListener(type, fn, false));
+
+  addClass = (v) => this.each((i) => (i.classList ? i.classList.add(v) : (i.className += ` ${v}`)));
+  toggleClass = (v) => this.each((i) => i.classList.toggle(v));
+  removeClass = (v) => this.each((i) => i.classList.remove(v));
+
+  html = (v) => this.each((i) => (i.innerHTML = v));
+  text = (v) => this.each((i) => (i.innerText = v));
+
+  insertBefore = (v) => this.each((i) => i.insertAdjacentHTML('beforeBegin', v));
+  insertAfter = (v) => this.each((i) => i.insertAdjacentHTML('afterEnd', v));
+  insertFirst = (v) => this.each((i) => i.insertAdjacentHTML('afterBegin', v));
+  insertLast = (v) => this.each((i) => i.insertAdjacentHTML('beforeEnd', v));
+
+  empty = () => this.each((i) => (i.innerHTML = ''));
+}
+
+const $ = (s) => new Dom(s);
+export default $;
