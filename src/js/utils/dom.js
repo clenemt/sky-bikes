651d3d00bac75022f2af72d5696b50c6d920722d
clenemt <clement.moron@gmail.com>
2018-06-20 01:49:51 +0200

    :fire: Remove some unused utils

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,5 +1,8 @@
 /* eslint arrow-body-style: off, no-param-reassign: off */
 
+/**
+ * A small jquery like abstraction for manipulating the DOM.
+ */
 class Dom {
   constructor(s) {
     if (typeof s === 'string')
@@ -12,34 +15,17 @@ class Dom {
     return this;
   };
 
-  css = (v) => this.each((i) => (i.style.cssText += v));
-  cssdom = (v) =>
-    this.each((i) => Object.keys(v).forEach((key) => (i.style[key] = v[key])));
-
-  on = (type, fn) => this.each((i) => i.addEventListener(type, fn, false));
-
-  addClass = (v) =>
-    this.each(
-      (i) => (i.classList ? i.classList.add(v) : (i.className += ` ${v}`)),
-    );
-  toggleClass = (v) => this.each((i) => i.classList.toggle(v));
-  removeClass = (v) => this.each((i) => i.classList.remove(v));
-
-  html = (v) => this.each((i) => (i.innerHTML = v));
-
-  insertBefore = (v) =>
-    this.each((i) => i.insertAdjacentHTML('beforeBegin', v));
-  insertAfter = (v) => this.each((i) => i.insertAdjacentHTML('afterEnd', v));
-  insertFirst = (v) => this.each((i) => i.insertAdjacentHTML('afterBegin', v));
-  insertLast = (v) => this.each((i) => i.insertAdjacentHTML('beforeEnd', v));
-
-  empty = () => this.each((i) => (i.innerHTML = ''));
   val = () =>
     Array.from(this.value[0].elements).reduce((result, el) => {
       if (!el.name || !el.type) return result;
       result[el.name] = el.value;
       return result;
     }, {});
+
+  on = (type, fn) => this.each((i) => i.addEventListener(type, fn, false));
+  html = (v) => this.each((i) => (i.innerHTML = v));
+  insertLast = (v) => this.each((i) => i.insertAdjacentHTML('beforeEnd', v));
+  empty = () => this.each((i) => (i.innerHTML = ''));
 }
 
 const $ = (s) => new Dom(s);
